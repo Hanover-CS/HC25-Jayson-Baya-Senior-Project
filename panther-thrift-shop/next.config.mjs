5013f5a1b734adbc5cfda668e4c1d51fbcfa6899
Jayson Baya <yesjaysonb@gmail.com>
2024-11-12 13:37:10 -0500

    I disabled SWC, so I can use .babelrc that I need for my Test. Click this for reference: https://nextjs.org/docs/messages/swc-disabled

--- panther-thrift-shop/next.config.mjs
+++ panther-thrift-shop/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true, // Force SWC transforms even if Babel is configured
+    },
+};
 
 export default nextConfig;
